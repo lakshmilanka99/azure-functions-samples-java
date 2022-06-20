59d63e03b8221b99e87283086433fdf2d2b27199
Shubin Jacob <shubin.jacob@aa.com>
2022-06-20 15:09:24 -0500

    added code changes

---
+++ src/main/java/com/functions/Hello.java
@@ -0,0 +1,14 @@
+package com.functions;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+@Component
+public class Hello implements Function<Mono<User>, Mono<Greeting>> {
+
+    public Mono<Greeting> apply(Mono<User> mono) {
+        return mono.map(user -> new Greeting("Hello, " + user.getName() + "!\n"));
+    }
+}
